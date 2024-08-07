07f3d6f47f8ce822e59c96dfb2e1fa18077d2142
Metanorm <metanorm@MetanormPro-485.local>
2024-08-06 21:06:11 -0700

    new noiseoverlay component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useKeenSlider } from 'keen-slider/react';
 import { ChakraProvider, Button, Flex, Link, extendTheme } from '@chakra-ui/react';
 import TopRight from './components/TopRight';
 import PunkIcon from "./components/Icon";
+import { posters } from './postersData';
 import 'keen-slider/keen-slider.min.css';
 import './styles.css';
 import { NoiseOverlay } from './components/NoiseOverlay';
@@ -23,7 +24,7 @@ const myTheme = extendTheme({
 });
 
 const Zorb = () => (
-  <svg width="693" height="693" viewBox="0 0 693 693" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="64" height="64" viewBox="0 0 693 693" fill="none" xmlns="http://www.w3.org/2000/svg">
 	<path d="M0 346.5C0 537.587 155.413 693 346.5 693C537.587 693 693 537.587 693 346.5C692.823 155.413 537.411 0 346.5 0C155.413 0 0 155.413 0 346.5Z" fill="url(#paint0_radial)" />
 	<defs>
 	  <radialGradient id="paint0_radial" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(450.164 168.797) scale(521.764)">
@@ -38,20 +39,6 @@ const Zorb = () => (
 	</defs>
   </svg>
 );
-
-const posters = [
-  { id: 1, backgroundColor: 'pink', hoverColor: 'chartreuse', href: 'https://create.zora.co/collections/0x6056d7d72830eec9e72e9969f25f5baa754c1434' },
-  { id: 2, backgroundColor: 'green', hoverColor: 'pink', href: 'https://create.zora.co/collections/0xf09b5a90903afc0f6e19437d0e18f9710e7b1d3e' },
-  { id: 3, backgroundColor: 'lightgray', hoverColor: 'yellow', href: 'https://create.zora.co/collections/0x0797215f34bcf6e3770e48a76d1978a0cf67b6e6' },
-  { id: 4, backgroundColor: 'blue', hoverColor: 'white', href: 'https://create.zora.co/collections/0x3462150aff5724e2d74d3118b7ffd6e963536ddd' },
-  { id: 5, backgroundColor: 'chartreuse', hoverColor: 'pink', href: 'https://create.zora.co/collections/0x892e712e7917c06bf3b73260c7a535ec006512d6' },
-  { id: 6, backgroundColor: 'orange', hoverColor: 'yellow', href: 'https://create.zora.co/collections/0x058905fde701d598a9922751478e9c3c7e19bf16' },
-  { id: 7, backgroundColor: 'green', hoverColor: 'pink', href: 'https://create.zora.co/collections/0x8b50cb0efe5a278367499c0932e6627048473647' },
-  { id: 8, backgroundColor: 'orange', hoverColor: 'pink', href: 'https://zora.co/collect/eth:0x67524c0d89d02bdf6ddff96125d785a666f810dc' },
-  { id: 9, backgroundColor: 'blue', hoverColor: 'green', href: 'https://zora.co/collect/zora:0x82737224d2a091cb0cb7c806e5a51794b5363ab8/9?referrer=0xE638cb3fA853622B2824CbDab3C27b06E8049651' },
-  { id: 10, backgroundColor: 'yellow', hoverColor: 'orange', href: 'https://zora.co/collect/zora:0x82737224d2a091cb0cb7c806e5a51794b5363ab8/10?referrer=0xE638cb3fA853622B2824CbDab3C27b06E8049651' },
-  { id: 11, backgroundColor: 'green', hoverColor: 'pink', href: 'https://zora.co/collect/zora:0x82737224d2a091cb0cb7c806e5a51794b5363ab8/11?referrer=0xE638cb3fA853622B2824CbDab3C27b06E8049651' },
-];
 
 const WheelControls = (slider) => {
   let touchTimeout;
@@ -103,7 +90,6 @@ const WheelControls = (slider) => {
 const PosterButton = ({ backgroundColor, hoverColor, href, text }) => (
   <Flex pos="fixed" bottom="20vh" left="-1vh" textTransform="uppercase" transform="rotate(-90deg)">
 	<Button
-	  transform="scaleY(2.2) scale(.666)"
 	  flexDirection="row"
 	  backgroundColor={backgroundColor}
 	  borderRadius="36"
@@ -112,12 +98,14 @@ const PosterButton = ({ backgroundColor, hoverColor, href, text }) => (
 	  target="_blank"
 	  rel="noopener noreferrer"
 	  variant="button"
-	  rightIcon={<Zorb />}
 	  _hover={{ bg: hoverColor, color: backgroundColor }}
 	>
-	  <Link href={href} variant="none" isExternal>
-		<p>{text}</p>
-	  </Link>
+	  <Flex alignItems="center">
+		<Zorb />
+		<Link href={href} variant="none" isExternal>
+		  <p style={{ transform: 'scaleY(2.4) scale(.666)', marginLeft: '8px' }}>{text}</p>
+		</Link>
+	  </Flex>
 	</Button>
   </Flex>
 );
